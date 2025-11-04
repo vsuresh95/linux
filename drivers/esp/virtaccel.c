6a8cf33c0dcb44641a63c55560284561b45adbe8
Shanbo Zhang <EscapistArcadia@outlook.com>
2025-11-04 09:07:01 -0600

    Support dynamic accelerator configuration option.

--- drivers/esp/virtaccel.c
+++ drivers/esp/virtaccel.c
@@ -16,7 +16,11 @@ extern int esp_wait(struct esp_device *esp);
 extern void esp_update_status(struct esp_device *esp);
 extern long esp_p2p_init(struct esp_device *esp, struct esp_access *access);
 
-static struct esp_device *mac_device;
+char accel_name[64] = "mac_stratus";
+module_param_string(accel_name, accel_name, sizeof(accel_name), 0644);
+/* change by: echo -n "<name>" | tee /sys/module/virtaccel/parameters/accel_name */
+
+// static struct esp_device *mac_device;
 
 SYSCALL_DEFINE1(clone_accel, struct esp_access *, accel_info_user) {
     /*
@@ -29,7 +33,7 @@ SYSCALL_DEFINE1(clone_accel, struct esp_access *, accel_info_user) {
     extern struct list_head esp_devices;
     extern struct esp_status esp_status;
 
-    struct esp_device *dev;
+    struct esp_device *dev = NULL, *entry;
     struct esp_access *access;
 	struct contig_desc *contig;
     int rc;
@@ -40,119 +44,118 @@ SYSCALL_DEFINE1(clone_accel, struct esp_access *, accel_info_user) {
      *   open(esp_thread_info_t::devname, O_RDWR, 0) => esp_open() in esp.ko;
      *   accelerator_thread_serial => ioctl(info->fd, info->ioctl_req, info->esp_desc) => esp_ioctl() in esp.ko;
      */
-    if (unlikely(!mac_device)) {                                                    /* obtain mac_device without esp_open() */
-        struct list_head *node;
-        
-        list_for_each(node, &esp_devices) {
-            dev = list_entry(node, struct esp_device, list);                        /* find mac_stratus device */
-            if (!strncmp(dev->driver->plat.driver.name, "mac_stratus", strlen("mac_stratus"))) {
-                mac_device = dev;
-                break;
-            }
-        }
-        if (unlikely(!mac_device)) {
-            // printk(KERN_ERR "clone_accel: cannot find mac_stratus device\n");
-            return -ENODEV;
+    // if (unlikely(!mac_device)) {                                                    /* obtain mac_device without esp_open() */
+    struct list_head *node;
+    
+    list_for_each(node, &esp_devices) {
+        entry = list_entry(node, struct esp_device, list);                          /* find mac_stratus device */
+        if (!strncmp(entry->driver->plat.driver.name, accel_name, strlen(accel_name))) {
+            dev = entry;
+            break;
         }
     }
+    if (unlikely(!dev)) {
+        printk(KERN_ERR "clone_accel: cannot find %s device\n", accel_name);
+        return -ENODEV;
+    }
+    // }
 
     // printk(KERN_INFO "clone_accel: mac_stratus device found at 0x%llx\n", (unsigned long long)mac_device);
     // printk(KERN_INFO "clone_accel: mac_stratus iomem at 0x%llx\n", (unsigned long long)mac_device->iomem);
 
     /* esp_open() */
-    if (!try_module_get(mac_device->module)) {
+    if (!try_module_get(dev->module)) {
         return -ENODEV;
     }
 
     /* esp_ioctl() */
-    access = kmalloc(mac_device->driver->arg_size, GFP_KERNEL);
+    access = kmalloc(dev->driver->arg_size, GFP_KERNEL);
     if (access == NULL) {
-        module_put(mac_device->module);
+        module_put(dev->module);
         return -ENOMEM;
     }
 
-    if (copy_from_user(access, accel_info_user, mac_device->driver->arg_size)) {
-        kfree(access);
-        module_put(mac_device->module);
-        return -EFAULT;
+    if (copy_from_user(access, accel_info_user, dev->driver->arg_size)) {
+        rc = -EFAULT;
+        goto clone_accel_out;
     }
 
 	contig = contig_khandle_to_desc(access->contig);
 	if (contig == NULL) {
 		rc = -EFAULT;
-		goto out;
+		goto clone_accel_out;
 	}
 	if (access->p2p_nsrcs > 4) {
 		rc = -EINVAL;
-		goto out;
+		goto clone_accel_out;
 	}
 
-	if (!esp_xfer_input_ok(mac_device, contig)) {
+	if (!esp_xfer_input_ok(dev, contig)) {
 		rc = -EINVAL;
-		goto out;
+		goto clone_accel_out;
 	}
 
-	if (mac_device->driver->xfer_input_ok && !mac_device->driver->xfer_input_ok(mac_device, access)) {
+	if (dev->driver->xfer_input_ok && !dev->driver->xfer_input_ok(dev, access)) {
 		rc = -EINVAL;
-		goto out;
+		goto clone_accel_out;
 	}
 
-	if (mutex_lock_interruptible(&mac_device->lock)) {
+	if (mutex_lock_interruptible(&dev->lock)) {
 		rc = -EINTR;
-		goto out;
+		goto clone_accel_out;
 	}
 
-	rc = esp_p2p_init(mac_device, access);
+	rc = esp_p2p_init(dev, access);
 	if (rc)
-		goto out;
+		goto clone_accel_out;
 
-	mac_device->coherence = access->coherence;
-	mac_device->footprint = access->footprint;
-    mac_device->alloc_policy = access->alloc_policy;
-    mac_device->ddr_node = access->ddr_node;
-	mac_device->in_place = access->in_place;
-	mac_device->reuse_factor = access->reuse_factor;
+	dev->coherence = access->coherence;
+	dev->footprint = access->footprint;
+    dev->alloc_policy = access->alloc_policy;
+    dev->ddr_node = access->ddr_node;
+	dev->in_place = access->in_place;
+	dev->reuse_factor = access->reuse_factor;
 
     if (mutex_lock_interruptible(&esp_status.lock)) {
         rc = -EINTR;
-        goto out;
+        goto clone_accel_out;
     }
 
-    esp_runtime_config(mac_device);
+    esp_runtime_config(dev);
 
     mutex_unlock(&esp_status.lock);
 
-	rc = esp_flush(mac_device);
+	rc = esp_flush(dev);
 	if (rc)
-		goto out;
+		goto clone_accel_out;
 
-	esp_transfer(mac_device, contig);
+	esp_transfer(dev, contig);
 
-	if (mac_device->driver->prep_xfer)
-		mac_device->driver->prep_xfer(mac_device, access);
+	if (dev->driver->prep_xfer)
+		dev->driver->prep_xfer(dev, access);
 
 	if (access->run) {
         if (access->start_stop) {
-           esp_run(mac_device);
+           esp_run(dev);
         } else {
-           esp_run(mac_device);
-           rc = esp_wait(mac_device);
+           esp_run(dev);
+           rc = esp_wait(dev);
         }
 	}
 
     if (mutex_lock_interruptible(&esp_status.lock)) {
         rc = -EINTR;
-        goto out;
+        goto clone_accel_out;
     }
 
-    esp_update_status(mac_device);
+    esp_update_status(dev);
 
     mutex_unlock(&esp_status.lock);
 
-	mutex_unlock(&mac_device->lock);
+	mutex_unlock(&dev->lock);
 
-out:
-    module_put(mac_device->module);
+clone_accel_out:
+    module_put(dev->module);
 	kfree(access);
 	return rc;
 }
